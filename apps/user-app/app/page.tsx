dcd4fb48dd8c7f2046671cd2a7dcc121d7a302ba
namastegit <akshaykailey1507@gmail.com>
2024-04-12 11:27:16 +0530

    checking pipeline

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -11,11 +11,11 @@
 //   );
 // }
 
-import { getServerSession } from "next-auth";
-import { redirect } from 'next/navigation'
+i { getServerSession } from "next-auth";
+im { redirect } from 'next/navigation'
 import { authOptions } from "./lib/auth";
 
-export default async function Page() {
+exp default async function Page() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
     redirect('/dashboard')
